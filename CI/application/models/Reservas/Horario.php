f972ba8196df4e0f813da1e32a892c0ca9af2803
br1pop <brunopoppolo@hotmail.com>
2015-10-29 19:20:54 -0300

    Horarios Dominio

---
+++ CI/application/models/Reservas/Horario.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Horario
+ *
+ * @author LoLo
+ */
+class Horario extends CI_Model{
+    
+    private $inicio;
+    private $fin;
+    
+    public function __construct($pini="",$pfin="") {
+        parent::__construct();
+        $this->inicio = $pini;
+        $this->fin = $pfin;
+    }
+    
+    function getInicio() {
+        return $this->inicio;
+    }
+
+    function getFin() {
+        return $this->fin;
+    }
+    
+    public function estaEnHorario($hora){
+        return ($this->inicio < $hora && $hora < $this->fin);
+    }
+
+
+    
+}
---
+++ CI/application/models/Reservas/SSReservas.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of SSReservas
+ *
+ * @author LoLo
+ */
+class SSReservas extends CI_Model {
+
+    private static $instancia;
+    private $dias = array();//no tiene sentido??
+
+    public static function getInstancia() {
+        if (!self::$instancia instanceof self) {
+            self::$instancia = new self;
+        }
+        return self::$instancia;
+    }
+    
+    public function __construct() {
+        parent::__construct();
+        $this->load->model('Reservas/HorariosBD');
+    }
+    
+    public function obtenerHorario($pusuario,$pnombre,$dia){
+        return HorariosBD::getHorarios($dia,$pusuario,$pnombre);
+    }
+    
+    public function agregarHorario($pDia,$pIni,$pFin,$pusuario,$pnombre){        
+        if($this->dias($pDia) == null){//no tiene sentido??
+            //$this->dias($pDia) = new Dia();
+        }
+        $this->dias($pDia)->agregarHorario($pIni,$pFin,$pusuario,$pnombre);        
+    }    
+
+}
